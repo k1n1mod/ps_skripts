e67ddbeef32a97b788116890e15fad2b5590bff5
K1N1MOD <106468699+k1n1mod@users.noreply.github.com>
2023-01-30 10:32:37 +0100

    Add files via upload

---
+++ Check_Service_restart_it_and_create_logfile.ps1
@@ -0,0 +1,39 @@
+﻿param(
+    $ServiceName
+)
+
+$ServiceName = $ServiceName
+$LogDirectory = "Path"
+$LogFile = Join-Path $LogDirectory "ServiceRestart.log"
+
+
+if (!(Test-Path $LogDirectory)) {
+  New-Item $LogDirectory -ItemType Directory
+}
+
+$Service = Get-Service -Name $ServiceName
+
+if ($Service.Status -eq "Stopped") {
+  if (Test-Path $LogFile) {
+    $LastLine = Get-Content $LogFile | Select-Object -Last 1
+    if ($LastLine.Contains($ServiceName)) {
+      $Count = [int]$LastLine.Split(' ')[-1]
+      $Count++
+    } else {
+      $Count = 0
+    }
+  }
+
+  $Service | Start-Service
+  $Timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+  "$Timestamp The service '$ServiceName' was restarted. Count: $Count" | Out-File -Append $LogFile
+}
+
+Get-Content $LogFile | ForEach-Object {
+  $LogDate = [datetime]::ParseExact($_.Split(' ')[0], "yyyy-MM-dd", $null)
+  if ($LogDate -lt (Get-Date).AddDays(-7)) {
+    $_ | Out-File $LogFile -Encoding UTF8 -Force -Append
+  } else {
+    $_
+  }
+}
