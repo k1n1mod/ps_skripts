5a5888520c068930d4527793d729623a4fb14435
K1N1MOD <106468699+k1n1mod@users.noreply.github.com>
2023-01-30 10:32:24 +0100

    Add files via upload

---
+++ Check_logs_with_alerting.ps1
@@ -0,0 +1,18 @@
+﻿$LogDirectory = "Path\Logs"
+$LogFile = Join-Path $LogDirectory "ServiceRestart.log"
+
+$RestartEntries = Get-Content $LogFile | Select-Object -Last 5
+
+foreach ($RestartEntry in $RestartEntries) {
+  $Timestamp = $RestartEntry.Split(' ')[0]
+  $ServiceName = $RestartEntry.Split(' ')[4]
+  $Count = $RestartEntry.Split(' ')[-1]
+
+  if ($Count -gt 4) {
+    $warning = "The service '$ServiceName' has been restarted $Count times. Last restart was on $Timestamp"
+    Ninja-Property-Set servicealert ($warning) 
+  } else {
+    $warning = ""
+    Ninja-Property-Set servicealert ($warning)
+  }
+}
